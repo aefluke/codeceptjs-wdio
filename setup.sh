283ee548feb9d5676f9c3baaf9ec6b40c9db51cc
aefluke <ahmet6yilmaz@gmail.com>
2021-02-22 12:06:23 +0300

    npm install command added to bash

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 
 echo hello world
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install git
-brew install docker
 brew install node
+brew install docker
+npm install
 docker pull codeception/codeceptjs
 docker-compose up -d
 
