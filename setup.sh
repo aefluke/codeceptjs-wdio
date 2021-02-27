2ddb55b9deb9d375d7bb4ce2ea524bdac761a5df
aefluke <ahmet6yilmaz@gmail.com>
2021-02-27 21:31:57 +0300

    docker needs to be installed with cask

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 echo hello world
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install node
-brew install docker
+brew install --cask docker
 npm install
 docker pull codeception/codeceptjs
 docker-compose up -d
