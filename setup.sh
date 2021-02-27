f10c28f9c497823045454d9304eefa2a5e6dacf9
aefluke <ahmet6yilmaz@gmail.com>
2021-02-27 21:34:19 +0300

    chrome install command added to setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ echo hello world
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install node
 brew install --cask docker
+brew install --cask google-chrome
 npm install
 docker pull codeception/codeceptjs
 docker-compose up -d
